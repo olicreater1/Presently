1e16a66c4c8614e4da5d797d5380cf2d359ca103
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2025-09-24 21:38:17 -0400

    login base

---
+++ login.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="login-div">
+            <h1>Login Sucessful</h1>
+            <h2>Welcome <?php echo $_POST["username"]; ?></h2>
+        </div>
+    </body>
+</html>
